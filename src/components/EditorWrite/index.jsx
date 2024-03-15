3344dc13762cc352495e6598f47dc22e70c2c5ba
tjd985 <tjd985@gmail.com>
2024-03-15 22:40:20 +0900

    ✨ [Feat] 키보드 이벤트 구현
    유저가 cmd + S 누를시 이벤트핸들러 동작 구현

--- src/components/EditorWrite/index.jsx
+++ src/components/EditorWrite/index.jsx
@@ -1,10 +1,54 @@
 import styled from "styled-components";
+import { useNavigate, useParams } from "react-router-dom";
+
+import saveCurrentCode from "../services/saveCurrentCode";
+
+import CONSTANTS from "../../constants/constants";
+
+const { KEYBOARD_STATUS, KEY_CMD, KEY_S } = CONSTANTS;
 
 function EditorWrite({ handleChange, value }) {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  async function handleKeyDown(ev) {
+    if (ev.type === "keydown") {
+      KEYBOARD_STATUS[ev.keyCode] = true;
+    }
+
+    if (ev.type === "keyup") {
+      KEYBOARD_STATUS[ev.keyCode] = false;
+    }
+
+    if (KEYBOARD_STATUS[KEY_CMD] && KEYBOARD_STATUS[KEY_S]) {
+      ev.preventDefault();
+      const currentCode = ev.target.value;
+
+      const requestResult = await saveCurrentCode(currentCode, id);
+
+      if (requestResult.result === "Error") {
+        return;
+      }
+
+      KEYBOARD_STATUS[KEY_CMD] = false;
+      KEYBOARD_STATUS[KEY_S] = false;
+
+      const { _id: temporaryUserId, version, code } = requestResult.content;
+
+      navigate(`/id/${temporaryUserId}/version/${version}`, {
+        state: {
+          code,
+        },
+      });
+    }
+  }
+
   return (
     <CustomEditorWrite
       className="editor-write"
       onChange={handleChange}
+      onKeyDown={handleKeyDown}
+      onKeyUp={handleKeyDown}
       value={value}
     />
   );
