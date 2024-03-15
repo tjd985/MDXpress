b267a97f0a38e9072f7ae865a31cc348103132c9
tjd985 <tjd985@gmail.com>
2024-03-15 16:30:22 +0900

    ✨ [Feat] Preview 구현
    에러 발생시, ErrorBoundary를 이용한 에러핸들링 및 실시간 Preview 구현

--- src/components/Preview/index.jsx
+++ src/components/Preview/index.jsx
@@ -1,15 +1,34 @@
+import { useMemo } from "react";
 import styled from "styled-components";
 
-function Preview() {
-  return <Container>this is Preview</Container>;
+import ErrorBoundary from "../shared/ErrorBoundary";
+import ErrorFallback from "../shared/ErrorFallback";
+
+function Preview({ previewComponent }) {
+  const key = useMemo(() => Math.random().toString(), [previewComponent]);
+
+  return (
+    <Container id="preview">
+      <ErrorBoundary key={key} FallbackComponent={ErrorFallback}>
+        {previewComponent}
+      </ErrorBoundary>
+    </Container>
+  );
 }
 
 const Container = styled.section`
+  box-sizing: border-box;
+
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-grow: 1;
   width: 100%;
-  height: 100%;
+  height: 70vh;
+  border: 1px solid #000;
+  padding: 10px;
+  margin: 10px;
 `;
 
 export default Preview;
