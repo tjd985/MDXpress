1bdb8937794a63eaa295191f4c1a4b26f75f2d80
tjd985 <tjd985@gmail.com>
2024-03-15 16:23:49 +0900

    ✨ [Feat] mdx to js compile기능 구현
    유저가 입력한 mdx문자열을 컴파일해서 JSX컴포넌트를 추출하는 로직 및 컴포넌트 구현

--- src/components/Editor/index.jsx
+++ src/components/Editor/index.jsx
@@ -1,15 +1,73 @@
+import { useState, useEffect } from "react";
 import styled from "styled-components";
+import { compile, run } from "@mdx-js/mdx";
+import * as jsxRuntime from "react/jsx-runtime";
 
-function Editor() {
-  return <Container>This is Editor</Container>;
+import EditorView from "../EditorView";
+import EditorWrite from "../EditorWrite";
+import ErrorFallback from "../shared/ErrorFallback";
+
+function MDXEditor({ setPreview }) {
+  const [userCode, setUserCode] = useState("");
+
+  const compileToJs = async () => {
+    try {
+      const compiledCode = await compile(userCode, {
+        outputFormat: "function-body",
+      });
+      const result = await run(compiledCode.value, jsxRuntime);
+      const MDXContent = result.default;
+
+      setPreview(<MDXContent />);
+    } catch (error) {
+      setPreview(<ErrorFallback error={error} />);
+    }
+  };
+
+  useEffect(() => {
+    compileToJs();
+
+    return;
+  }, [userCode]);
+
+  function updateUserCode(ev) {
+    setUserCode(ev.target.value);
+  }
+
+  return (
+    <EditorContainer className="editor">
+      <EditorInner className="editor-inner">
+        <EditorWrite handleChange={updateUserCode} value={userCode} />
+        <EditorView userCode={userCode} />
+      </EditorInner>
+    </EditorContainer>
+  );
 }
 
-const Container = styled.section`
+const EditorContainer = styled.section`
+  box-sizing: border-box;
+
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 100%;
+  min-height: 50vh;
 `;
 
-export default Editor;
+const EditorInner = styled.div`
+  position: relative;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 70vh;
+  margin: 10px;
+
+  background-color: #1c1d21;
+  color: white;
+`;
+
+export default MDXEditor;
