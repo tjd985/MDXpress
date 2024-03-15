c6e48a9724cfa5751ed36b5e0fe4f33d97ae6545
tjd985 <tjd985@gmail.com>
2024-03-15 15:58:21 +0900

    ♻️ [Refactor] styled-components 구문 삭제
    styled-componenets구문 삭제

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,14 +1,11 @@
 import { Route, Routes, Navigate } from "react-router-dom";
-import styled from "styled-components";
-import { Reset } from "styled-reset";
 
-import Layout from "./Layout";
-import Home from "./Home";
+import Layout from "../Layout";
+import Home from "../Home";
 
 function App() {
   return (
     <>
-      <Reset />
       <Layout />
       <Routes>
         <Route path="/id/:id/version/:version" element={<Home />} />
