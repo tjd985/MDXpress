83389b216f052c9d80f48fbc0fb91c842104d255
tjd985 <tjd985@gmail.com>
2024-03-18 03:06:33 +0900

    🔥 [Remove] Layout 컴포넌트 삭제
    Layout 컴포넌트 삭제

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 
-import Layout from "../Layout";
 import Home from "../Home";
+import Header from "../Header";
 
 function App() {
   return (
     <>
-      <Layout />
+      <Header />
       <Routes>
         <Route path="/id/:id/version/:version" element={<Home />} />
         <Route path="/" element={<Navigate to="/id/:id/version/:version" />} />
