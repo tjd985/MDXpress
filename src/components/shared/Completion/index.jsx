70702520e0cc54c7f3a42d56731821a8c6432b19
tjd985 <tjd985@gmail.com>
2024-03-18 16:40:56 +0900

    ✨ [Feat] 완료 컴포넌트 구현
    모달 내에 들어갈 완료 컴포넌트 구현

---
+++ src/components/shared/Completion/index.jsx
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+import Description from "../Description";
+
+function Completion({
+  imageSrc,
+  title,
+  description,
+  handleClick,
+  linkURL = null,
+}) {
+  return (
+    <Wrapper>
+      <Title>{title}</Title>
+      <CompleteImage src={imageSrc} alt="complete image" />
+      <Description text={description} />
+      {linkURL && (
+        <LinkSection>
+          your Link <em>{linkURL}</em>
+        </LinkSection>
+      )}
+      <CloseButton onClick={handleClick}>Okay</CloseButton>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 500px;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 20px;
+
+  color: #000;
+`;
+
+const CompleteImage = styled.img`
+  width: 200px;
+  margin-bottom: 20px;
+`;
+
+const LinkSection = styled.div`
+  margin-top: 20px;
+
+  text-align: center;
+  color: #000;
+
+  em {
+    display: block;
+    padding: 10px;
+    border-radius: 10px;
+
+    background-color: #f0e68c;
+    font-weight: bold;
+  }
+`;
+
+const CloseButton = styled.button`
+  min-width: 300px;
+  min-height: 50px;
+  padding: 12px 24px;
+  border: 2px solid #000000;
+  border-radius: 4px;
+  margin-top: 50px;
+
+  font-size: 1rem;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+export default Completion;
