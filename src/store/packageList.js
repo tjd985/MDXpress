347a1ee04ba39160f259f4f3f54f06591897dbaa
tjd985 <tjd985@gmail.com>
2024-03-20 17:12:43 +0900

    ✨ [Feat] packgeList 스토어 구현
    유저가 설치한 packageList를 관리하는 스토어

---
+++ src/store/packageList.js
@@ -0,0 +1,30 @@
+import { create } from "zustand";
+
+function packageStore(set) {
+  return {
+    packageList: {},
+    setPackage: packageInformation => {
+      const [packageName, version] = packageInformation.split(" ");
+
+      set(state => {
+        state.packageList = {
+          ...state.packageList,
+          [packageName]: version,
+        };
+
+        return state;
+      });
+    },
+    clearPackageList: () => {
+      set(state => {
+        state.packageList = {};
+
+        return state;
+      });
+    },
+  };
+}
+
+const usePackageStore = create(packageStore);
+
+export default usePackageStore;
