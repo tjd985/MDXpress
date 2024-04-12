ffe7b5b5a637f569bf3662615f9b57b7964845a9
tjd985 <tjd985@gmail.com>
2024-04-12 23:45:56 +0900

    ✨ [Feat] 모바일 반응형 대응
    모바일로 접속 시 MobileNotification 렌더링

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,18 +1,50 @@
 import { Route, Routes, Navigate } from "react-router-dom";
+import styled from "styled-components";
 
 import Home from "../Home";
 import Header from "../Header";
+import Description from "../shared/Description";
 
 function App() {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/id/:id/version/:version" element={<Home />} />
-        <Route path="/" element={<Navigate to="/id/:id/version/:version" />} />
-      </Routes>
+      <Main>
+        <Header />
+        <Routes>
+          <Route path="/id/:id/version/:version" element={<Home />} />
+          <Route
+            path="/"
+            element={<Navigate to="/id/:id/version/:version" />}
+          />
+        </Routes>
+      </Main>
+      <MobileNotification>
+        <Description
+          className="mobile-notification"
+          text="MDXpress does not support mobile experiences. 😢\n
+          Please connect via PC or Tablet"
+        />
+      </MobileNotification>
     </>
   );
 }
+
+const Main = styled.div`
+  @media screen and (max-width: 600px) {
+    display: none;
+  }
+`;
+
+const MobileNotification = styled.div`
+  @media screen and (max-width: 600px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  display: none;
+`;
 
 export default App;
