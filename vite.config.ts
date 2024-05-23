8f48bbaa3c09402d8ab97f95bed2ad3cc9e5ef86
tjd985 <tjd985@gmail.com>
2024-05-23 17:17:51 +0900

    📝 [Docs] defineConfig 타입추가
    defineConfig 타입단언

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -17,4 +17,4 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-});
+} as UserConfig);
