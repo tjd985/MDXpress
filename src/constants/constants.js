bccba1d546f1b8bff2603ee268e6a225b1e87242
tjd985 <tjd985@gmail.com>
2024-06-13 21:53:49 +0900

    ♻️ [Refactor] 상수 추가
    WELCOME_MESSAGE 상수 추가

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -8,6 +8,10 @@ const CONSTANTS = {
     s: false,
   },
   TOAST_TIME: 3000,
+  WELCOME_MESSAGE:
+    "1. Writing MDX on the left editor will render it in real time on the right!\\n" +
+    "2.Use cmd + s to save your current code, and share it with your friends through a link!\\n" +
+    "3. If there's a third-party library you'd like to use, you can install and use it!",
 };
 
 export default CONSTANTS;
